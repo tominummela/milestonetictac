103c1ac46abd8b641235fb3806d61e61e7d05eeb
Tomi Nummela <tomi.nummela@gmail.com>
2019-10-11 13:24:44 +0300

    push for weekend progress

--- tictacgame.py
+++ tictacgame.py
@@ -6,7 +6,7 @@ def display_board(board):
     print(board[1]+"|"+board[2]+"|"+board[3])
 
 test_board = ["#","X","O","X","O","X","O","X","O","X"]
-
+tut_board = ["#","1","2","3","4","5","6","7","8","9"]
 
 def player_input():
     marker = ""
@@ -24,18 +24,18 @@ def place_marker(board,marker,position):
 
 def win_check(board, mark):
     #Horizontal winchecks
-    return ((board[7] == mark and board[8] == mark and board[9] or
-        board[4] == mark and board[5] == mark and board[6] or
-        board[1] == mark and board[2] == mark and board[3] or
+    return ((board[7] == mark and board[8] == mark and board[9]) or
+            (board[4] == mark and board[5] == mark and board[6]) or
+            (board[1] == mark and board[2] == mark and board[3]) or
     #Vertical winchecks
-        board[1] == mark and board[4] == mark and board[7] or
-        board[2] == mark and board[5] == mark and board[8] or
-        board[3] == mark and board[6] == mark and board[9] or
+            (board[1] == mark and board[4] == mark and board[7]) or
+            (board[2] == mark and board[5] == mark and board[8]) or
+            (board[3] == mark and board[6] == mark and board[9]) or
     #Diagonal winchecks
-        board[1] == mark and board[5] == mark and board[9] or
-        board[3] == mark and board[5] == mark and board[7] or
+            (board[1] == mark and board[5] == mark and board[9]) or
+            (board[3] == mark and board[5] == mark and board[7]))
 
-
+import random
 def choose_first():
     if random.randint(0,1) == 0:
         return "Player 2"
@@ -47,16 +47,16 @@ def space_check(board, position):
     return board[position] == " "
 
 def board_full_check(board):
-    for i in range(1,10)
+    for i in range(1,10):
         if space_check(board, i):
-        return False
+            return False
     return True
 
 
 def player_choice(board):
     position = 0
     while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board,position):
-        position = int(input("Choose your next move: 1-9") "))
+        position = int(input("Choose your next move: 1-9"))
     return position
 
 def replay():
@@ -67,3 +67,16 @@ def replay():
 #---------------------------------THE GAME------------------------------------
 #-----------------------------------------------------------------------------
 
+print("Welcome to Tic Tac Toe!")
+print("The boards layout is like a numpad:")
+(display_board(tut_board))
+
+# Reset board to all empty
+
+#Ask who goes first or random
+
+# Start the game, check for win condition and full board with while
+
+#Alternate turns between p1 and p2 (X and O)
+
+#Congratulate winner and ask for replay
